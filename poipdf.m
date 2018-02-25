79a42d1e4ec669680bbed130818aa9a3de873df7
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-02-25 13:33:24 +0100

    Add files via upload

---
+++ poipdf.m
@@ -0,0 +1,18 @@
+function poisson = poipdf(lambda,X)
+%This function will return you the value of the poisson probability density
+%function.
+%Matlab has it's own built in function called poisspdf. We can verify the
+%result by using poisspdf.
+
+%created by: shahrear
+%student.eco86@gmail.com
+
+if  nargin < 2
+    disp('error: number of input should be two');
+else
+    if isscalar(lambda)~= 1
+        disp('error: the value of the parameter should be a scalar');
+    end
+end
+poisson = ((exp(-lambda))*(lambda).^X) ./ factorial(X);
+end
---
+++ setval.m
@@ -0,0 +1,40 @@
+function X = setval(X,col,m)
+%This function will help you to replace the value of the  dummy variables
+%from 1 to any other numbers. 
+
+%Prepared by shahrear
+%student.eco86@gmail.com
+%07.08.2017-08.08.2017
+
+%%%%%%%%%%%%%%%%%%% 
+%[r c]=size(X);
+%tn=r*c;
+%1:1*r
+%1*r+1 : 2*r
+%2*r+1: 3*r
+%..........
+%(c-1)*(r+1)  : c*r
+%%%%%%%%%%%%%%%%%%%
+
+if nargin ==~3
+disp('error: input value should be three')
+end
+if nargin==3
+if isscalar(col)~=1||isscalar(m)~=1
+disp('error: number of the columns and replaced value of the dummy should be a scalar')
+else
+[r c]=size(X);
+if col > c
+disp('error:exceeds the number of the columns');
+else
+X((((col-1)*r)+1)  : col*r) = m.*X((((col-1)*r)+1)  : col*r);
+end
+end
+end
+
+
+
+
+
+
+ 
