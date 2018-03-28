a87e37a1d0877117da89e0be63a708c5c1604ea0
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-03-28 17:27:03 +0200

    Add files via upload

---
+++ gamdf.m
@@ -0,0 +1,26 @@
+function gmdf = gamdf(x,a,b)
+
+%This function will help you to create a gamma probability density function
+%However MatLab has its own function gampdf. To create a gamma function we have used 
+%here function gamf.
+
+%created by: shahrear
+%student.eco86@gmail.com
+
+
+if isscalar(a)~=1||isscalar(b)~=1
+       disp('error: the value of the parameter should be a scalar')
+end
+
+if nargin < 3
+    b=1;
+end
+
+if  nargin < 2
+    disp('error: number of input should be atleast two');
+end
+
+a(a<=0)=NaN;
+b(b<=0)=NaN;
+gmdf = exp((a-1)*log(x)- a.*log(b) - log(gamf(a)) - x./b);
+double(gmdf)
