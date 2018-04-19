730148be0436620aee95237196a4bf3c73f6aafc
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-04-19 17:15:56 +0200

    Add files via upload

---
+++ mserror.m
@@ -0,0 +1,23 @@
+function MSE = mserror(x,y,cons,beta) 
+%created by: shahrear
+%student.eco86@gmail.com
+%reference: https://en.wikipedia.org/wiki/Mean_squared_error
+
+% within sample mse of the predictor
+% x is the matirics of the exogenous variable
+% y is the vector of observed values of the variable being predicted  
+% cons is the vector of the constant
+% beta is the vector of the co-efficients 
+% yhat is the vector the n predictions 
+         n=size(x);
+         N=n(1);
+         cons=cons;
+         yhat = cons + x*beta';
+         MSE = sum((yhat - y).^(2))/N
+end
+
+
+
+
+
+
