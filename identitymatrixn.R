03d6336897fc2001d7086b653c8ce8b1385a2ab3
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-12-03 10:42:07 +0100

    Update identitymatrixn.R

--- identitymatrixn.R
+++ identitymatrixn.R
@@ -1,6 +1,7 @@
 created by: shaharear
 (c) Md. Shahrear Zaman
 student.eco86@gmail.com
+# This function was rejected as an incorrect solution from the statistics department of uni kiel..... :) 
 # please check what ever this function is correct or not before you will start to use 
 identitymatrix = function(n,m){
     I = matrix(0,nrow = n, ncol = m)
