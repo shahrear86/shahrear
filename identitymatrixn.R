cdd44c8704e455700f33c9d7c8912f262ca27aa8
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-12-03 10:34:39 +0100

    Update identitymatrixn.R

--- identitymatrixn.R
+++ identitymatrixn.R
@@ -4,9 +4,9 @@ student.eco86@gmail.com
 
 identitymatrix = function(n,m){
     I = matrix(0,nrow = n, ncol = m)
-    for (i in 1:n) {# i is the number of the row
-       for (j in 1:m) {# j is the number of the column
-          if(i==j) I[i,j]=1 # each element of the diagonal (i=j) is 1
+    for (i in 1:n) {
+       for (j in 1:m) {
+          if(i==j) I[i,j]=1
        }
     }
  return(I) 
